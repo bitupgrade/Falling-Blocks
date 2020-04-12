6063ba1c83c3989b21d5709fbc66ab0dfb4ccfea
Stefan <bitupgrade@outlook.com>
2020-04-12 20:52:21 +0100

    added the code and the build for the game

---
+++ Code/Assets/Scripts/FallingBlock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBlock : MonoBehaviour
+{
+    public Vector2 speedMinMax;
+    float speed;
+
+    float visableHeightThreshold;
+
+    void Start()
+    {
+        speed = Mathf.Lerp(speedMinMax.x, speedMinMax.y, Difficulty.GetDifficultyPercent());
+        visableHeightThreshold = -Camera.main.orthographicSize - transform.localScale.y;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime, Space.World);
+        if (transform.position.y < visableHeightThreshold)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
